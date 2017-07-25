450346bd26f14d397ab95c7c921099ce56efc027
Andries Smit <mr.andries.smit@gmail.com>
2017-07-25 08:15:14 +0200

    Add app env to CI config

--- tests/scripts/DeployMxApp.ts
+++ tests/scripts/DeployMxApp.ts
@@ -18,48 +18,53 @@ const build = new BuildService(settings.apiUrl, settings.user, settings.key);
 const version: string = pkg.version;
 const widgetName: string = pkg.widgetName;
 
-const appName = "fobapp";
-const environment: EnvironmentMode = "Acceptance";
+const appName = settings.appName;
+const environment: EnvironmentMode = settings.environment;
 const branchName = settings.branchName;
 
 deployApp().then(success => process.exit(0), error => process.exit(1));
 
 async function deployApp() {
-    try {
-        console.log("Get branch details");
-        const branch = await deploy.getBranch(appName, branchName);
-        const latestBuiltRevision = branch.LatestTaggedVersion.substring(branch.LatestTaggedVersion.lastIndexOf(".") + 1);
-        if (latestBuiltRevision === branch.LatestRevisionNumber.toString()) {
-            console.log("It is not needed to build, as the latest revision is already built.");
-            return;
-        }
-        const versionWithoutRevision = branch.LatestTaggedVersion.substring(0, branch.LatestTaggedVersion.lastIndexOf("."));
-        console.log("Start build:", appName, branchName, latestBuiltRevision, versionWithoutRevision);
-        const buildAction = await build.startBuild(appName, branchName, latestBuiltRevision, versionWithoutRevision);
-        console.log("Wait for build:", appName, buildAction.PackageId);
-        const deployPackage = await build.waitForBuild(appName, buildAction.PackageId, 600);
-        if (deployPackage.Status !== "Succeeded") {
-            console.log("No build succeeded within 10 minutes.");
-            return;
-        }
-        console.log("Stop app:", appName, environment);
-        await deploy.stopApp(appName, environment);
-        console.log("Clean environment:", appName, environment);
-        await deploy.cleanApp(appName, environment);
-        console.log("Transport package:", appName, environment, buildAction.PackageId);
-        await deploy.transportPackage(appName, environment, buildAction.PackageId);
-        console.log("Start app:", appName, environment);
-        const startJob = await deploy.startApp(appName, environment);
-        console.log("Wait for startup:", appName, environment, startJob.JobId);
-        const started = await deploy.waitForStart(appName, environment, startJob.JobId, 600);
+    return new Promise<boolean>(async (resolve, reject) => {
+        try {
+            console.log("Get branch details");
+            const branch = await deploy.getBranch(appName, branchName);
+            const latestBuiltRevision = branch.LatestTaggedVersion.substring(branch.LatestTaggedVersion.lastIndexOf(".") + 1);
+            if (latestBuiltRevision === branch.LatestRevisionNumber.toString()) {
+                console.log("It is not needed to build, as the latest revision is already built.");
+                return;
+            }
+            const versionWithoutRevision = branch.LatestTaggedVersion.substring(0, branch.LatestTaggedVersion.lastIndexOf("."));
+            console.log("Start build:", appName, branchName, latestBuiltRevision, versionWithoutRevision);
+            const buildAction = await build.startBuild(appName, branchName, latestBuiltRevision, versionWithoutRevision);
+            console.log("Wait for build:", appName, buildAction.PackageId);
+            const deployPackage = await build.waitForBuild(appName, buildAction.PackageId, 600);
+            if (deployPackage.Status !== "Succeeded") {
+                console.log("No build succeeded within 10 minutes.");
+                return;
+            }
+            console.log("Stop app:", appName, environment);
+            await deploy.stopApp(appName, environment);
+            console.log("Clean environment:", appName, environment);
+            await deploy.cleanApp(appName, environment);
+            console.log("Transport package:", appName, environment, buildAction.PackageId);
+            await deploy.transportPackage(appName, environment, buildAction.PackageId);
+            console.log("Start app:", appName, environment);
+            const startJob = await deploy.startApp(appName, environment);
+            console.log("Wait for startup:", appName, environment, startJob.JobId);
+            const started = await deploy.waitForStart(appName, environment, startJob.JobId, 600);
 
-        if (started === true) {
-            console.log("App successfully started.");
-        } else {
-            console.error("Failed to startup");
+            if (started === true) {
+                console.log("App successfully started.");
+                resolve(true);
+            } else {
+                console.error("Failed to startup");
+                reject("Failed to startup");
+            }
+            console.log("Done");
+        } catch (error) {
+            console.error("Error deploying", error);
+            reject(error);
         }
-        console.log("Done");
-    } catch (error) {
-        console.error("Error deploying", error);
-    }
+    });
 }
--- tests/scripts/Settings.ts
+++ tests/scripts/Settings.ts
@@ -1,10 +1,13 @@
-// tslint:disable
+// tslint:disable:rule no-console no-angle-bracket-type-assertion
 import * as fs from "fs";
 import * as path from "path";
+import { EnvironmentMode } from "./DeploymentService";
 
 interface Settings {
     apiUrl: string;
+    appName: string;
     branchName: string;
+    environment: EnvironmentMode;
     key: string;
     password: string;
     projectId: string;
@@ -18,16 +21,18 @@ export const getSettings = (): Settings => {
     if (fs.existsSync(localSettingFile)) {
         console.log("Running with local settings");
         return require("./localSettings").settings; // TODO validate content of file
-    } else if (process.env.CI){
-        // TODO check travis running?
+    } else if (process.env.CI) {
         return {
-            apiUrl: process.env.API_URL,
-            branchName: process.env.BRANCH_NAME, // trunk
-            key: process.env.API_KEY,
+            apiUrl: process.env.MX_API_URL, // https://deploy.mendix.com/api/1
+            appName: process.env.MX_APP_NAME, // fobapp
+            branchName: process.env.MX_BRANCH_NAME, // trunk
+            environment: <EnvironmentMode>process.env.MX_ENVIRONMENT, // Acceptance
+// Secure Mendix account key with travis cli > travis encrypt MX_API_KEY=yourSecretKey --add env.global
+            key: process.env.MX_API_KEY, // xxxxxxxx-xxxx-xxxx-xxxxx-xxxxxxxxxxxx
 // Secure Mendix account password with travis cli > travis encrypt MX_PASSWORD=yourSecretPassword --add env.global
             password: process.env.MX_PASSWORD,
-            projectId: process.env.PROJECT_ID, // App ID like d424a4fd-9473-4b53-94a5-99ad227c2278
-            teamServerUrl: process.env.TEAM_SERVER_URL, // https://teamserver.sprintr.com;
+            projectId: process.env.MX_PROJECT_ID, // App ID like d424a4fd-9473-4b53-94a5-99ad227c2278
+            teamServerUrl: process.env.MX_TEAM_SERVER_URL, // https://teamserver.sprintr.com;
             user: process.env.MX_USER // Mendix account login: user@example.com;
         };
     } else {
